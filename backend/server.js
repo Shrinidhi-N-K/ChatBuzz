db31dcf33b5b7d3ef4e3dc4e1f5d15e9ece391f0
shrinidhi <snkulkarni2003@gmail.com>
2024-10-30 17:29:46 +0530

    Basic Structure using exress node react and chakraUI

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const { chats } = require('./data');
+const dotenv = require('dotenv');
+
+
+const app = express();
+dotenv.config();
+const port = process.env.PORT || 5000
+
+app.listen(port, console.log(`server listening at port ${port}`));
+
+app.get('/', (req, res)=>{
+    res.send("Welcome");
+});
+
+app.get('/api/chats', (req, res)=>{
+    res.send(chats);
+});
+
+app.get('/api/chats/:id', (req, res) => {
+    console.log(req.params.id);
+    const singleChat = chats.find(c => c._id === req.params.id);
+
+    if (!singleChat) {
+        return res.status(404).send({ message: "Chat not found" });
+    }
+
+    res.send(singleChat);
+});
---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
+import "./App.css"
+import Home from "./Pages/Home";
+import Chats from "./Pages/Chats";
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path='/' element={<Home />} exact/>
+        <Route path='/chats' element={<Chats />}/>
+      </Routes>
+
+    </div>
+  );
+}
+
+export default App;
