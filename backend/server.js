4435f9b240b533013fe5e425ed687c2ff7759d33
shrinidhi <snkulkarni2003@gmail.com>
2024-10-31 11:03:07 +0530

    added login and signup page and connected to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { chats } = require('./data');
 const dotenv = require('dotenv');
-
+const connectDB = require('./config/db');
 
 const app = express();
 dotenv.config();
+connectDB();
 const port = process.env.PORT || 5000
 
 app.listen(port, console.log(`server listening at port ${port}`));
